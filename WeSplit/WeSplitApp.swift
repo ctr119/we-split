ef84bffa6b2e2ca6dbfc836a8540f92a1e5bec64
Victor Barrios <victor.barrios@bestsecret.com>
2023-10-12 11:04:00 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Barrios, Victor on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
